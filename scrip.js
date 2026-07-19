2150460353158cc8cfba73a518d9077bd034b3cd
251020902147-jpg <251020902147@st.phenikaa-uni.edu.vn>
2026-07-19 13:04:40 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,22 @@
+
+function upDate(previewPic) {
+
+    console.log("Sự kiện onmouseover đã kích hoạt!");
+    console.log("Thuộc tính alt: " + previewPic.alt);
+    console.log("Thuộc tính source: " + previewPic.src);
+
+  
+    document.getElementById('image').innerHTML = previewPic.alt;
+
+  
+    document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+
+function unDo() {
+
+    document.getElementById('image').style.backgroundImage = "url('')";
+
+
+    document.getElementById('image').innerHTML = "Di chuột qua một hình ảnh bên dưới để hiển thị ở đây.";
+}
