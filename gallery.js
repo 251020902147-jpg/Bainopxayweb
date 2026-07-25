b9c30ba3d29a92af0079e2b699ebcd780a123bfe
251020902147-jpg <251020902147@st.phenikaa-uni.edu.vn>
2026-07-25 23:39:52 +0700

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,15 +1,15 @@
-/* Hàm này kích hoạt khi di chuột (onmouseover) hoặc dùng phím tab (onfocus) */
+// Hàm này kích hoạt khi di chuột (onmouseover) hoặc dùng phím tab (onfocus)
 function upDate(previewPic) {
     let imageDiv = document.getElementById("image");
     
     // Đổi hình nền của thẻ div thành ảnh nhỏ
     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
     
-    // Đổi đoạn text của thẻ div thành thuộc tính alt của ảnh nhỏ
-    imageDiv.innerHTML = previewPic.alt;
+    // Lấy thuộc tính alt của ảnh nhỏ và đặt vào trong span văn bản
+    imageDiv.innerHTML = "<span class='image-text'>" + previewPic.alt + "</span>";
 }
 
-/* Hàm này kích hoạt khi chuột rời đi (onmouseleave) hoặc bỏ chọn phím tab (onblur) */
+// Hàm này kích hoạt khi chuột rời đi (onmouseleave) hoặc bỏ chọn phím tab (onblur)
 function unDo() {
     let imageDiv = document.getElementById("image");
     
@@ -17,21 +17,15 @@ function unDo() {
     imageDiv.style.backgroundImage = "url('')";
     
     // Hardcode (Gắn cứng) lại đoạn text ban đầu
-    imageDiv.innerHTML = "Di chuột qua hoặc dùng phím Tab chọn một hình ảnh bên dưới để hiển thị tại đây.";
+    imageDiv.innerHTML = "<span class='image-text'>Di chuột qua hoặc dùng phím Tab để xem ảnh to.</span>";
 }
 
-/* Hàm này thêm thuộc tính tabindex động vào tất cả các ảnh nhỏ */
+// Thêm thuộc tính tabindex động
 function addTabIndex() {
-    // Thêm thông báo console.log để xác nhận sự kiện onload đã kích hoạt
     console.log("Sự kiện onload đã kích hoạt. Đang thiết lập tabindex cho các hình ảnh...");
-    
     let images = document.querySelectorAll('.preview');
-    
-    // Sử dụng vòng lặp for để lặp qua từng hình ảnh theo yêu cầu
     for (let i = 0; i < images.length; i++) {
         images[i].setAttribute('tabindex', '0');
     }
 }
-
-// Lắng nghe sự kiện onload của trang web để gọi hàm addTabIndex
 window.onload = addTabIndex;
